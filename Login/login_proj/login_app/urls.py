f4169f97fd53f826dca7bf51ce1337e3c74a5a76
Shwetal <more.shwetal777@gamil.com>
2022-08-21 21:36:53 +0530

    Sorted Url issue

--- Login/login_proj/login_app/urls.py
+++ Login/login_proj/login_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home',views.home, name='home'),
-    path('login/signup/',views.signup,name='signup'),
+    path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
     path('logout/',views.user_logout,name='logout'),
 
