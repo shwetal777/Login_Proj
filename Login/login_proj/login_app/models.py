c51488eb285924b639da9e8833ab457b3bfdf23a
Shwetal <more.shwetal777@gamil.com>
2022-08-22 00:06:05 +0530

    Sorted Url issue

--- Login/login_proj/login_app/models.py
+++ Login/login_proj/login_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class SignUp(models.Model):
-    username= models.CharField(max_length=100)
-    email=models.EmailField(max_length=254)
-    password=models.CharField(max_length=20)
-    address=models.CharField(max_length=250)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=20)
+    address = models.CharField(max_length=250)
 
     def __str__(self):
         return self.username
-        
--- Login/login_proj/login_app/views.py
+++ Login/login_proj/login_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login
@@ -6,13 +6,14 @@ from django.contrib.auth import authenticate, logout
 
 # Create your views here.
 
+
 def login(request):
 
     if request.method == 'POST':
-        username= request.POST['username']
-        password= request.POST['password']
+        username = request.POST['username']
+        password = request.POST['password']
 
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         html2 = "<html><body>No such user</body></html>"
 
         if user is not None:
@@ -24,16 +25,17 @@ def login(request):
 
         return render(request, 'login.html')
 
+
 def signup(request):
 
     if request.method == 'POST':
-        username= request.POST['username']
-        email= request.POST['email']
-        password1= request.POST['password1']
-        password2= request.POST['password2']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
         # address= request.POST['address']
         html = "<html><body>Confirm Password and Password should be same </body></html>"
-        html1= "<html><body>User Already present </body></html>"
+        html1 = "<html><body>User Already present </body></html>"
 
         if password1 != password2:
             return HttpResponse(html)
@@ -41,7 +43,8 @@ def signup(request):
             # for instance in SignUp.objects.all():
             #     if (instance.username == username) or (instance.email==email):
             #         return HttpResponse(html1)
-            signup=User.objects.create_user(username=username, email=email, password=password1)
+            signup = User.objects.create_user(
+                username=username, email=email, password=password1)
             signup.save()
 
             return redirect('login')
