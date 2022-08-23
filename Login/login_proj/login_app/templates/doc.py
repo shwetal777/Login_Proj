967aeec4c9caa97bdb29c0580d4d5d822bd0e52c
Shwetal <more.shwetal777@gamil.com>
2022-08-23 23:53:30 +0530

    minor update

--- Login/login_proj/login_app/templates/doc.py
+++ Login/login_proj/login_app/templates/doc.py
@@ -15,3 +15,17 @@ def update_tuple(val, tup, index):
 
 up = update_tuple(val=6,tup=(1,2,3,4,5), index=0)
 print(up)
+
+
+
+import random
+card_type=["diamond", "heart", "spade", "club"]
+card_point=["A","K","Q","J","2","3","4","5","6","7","8","9","10"]
+deck = []
+for i in range(len(card_point)):
+    for j in range(len(card_type)):
+        deck.append(f"{card_point[i]} of {card_type[j]}")
+# print(deck)
+
+print(random.choice(deck))
+# rand = random.randrange(card["diamond"])
